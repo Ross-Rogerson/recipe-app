f3aaabac402ece421bd00b9ed79f1565e6c6bd57
Ross Rogerson <ross.rogerson@hotmail.com>
2023-04-15 15:14:45 +0100

    user model created

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w75@!i17le8!h7onl7ekiwka%5kh^4v)n-sl&t70+gugp_)tq5'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +128,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'users.authentication.JWTAuthentication'
+    ]
+}
---
+++ users/views.py
@@ -0,0 +1,46 @@
+import jwt
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers.common import UserSerializer
+from rest_framework import status
+from rest_framework.exceptions import PermissionDenied
+from datetime import datetime, timedelta
+from django.conf import settings
+
+from lib.exceptions import exceptions
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+class RegisterView(APIView):
+
+    # REGISTER ROUTE: POST /api/auth/register/
+    @exceptions
+    def post(self, request):
+        user_to_add = UserSerializer(data=request.data)
+        user_to_add.is_valid(raise_exception=True)
+        user_to_add.save()
+        return Response(user_to_add.data, status.HTTP_201_CREATED)
+    
+
+class LoginView(APIView):
+
+    # LOGIN ROUTE: POST /api/auth/login/
+    @exceptions
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+        user_to_login = User.objects.get(email=email)
+        if not user_to_login.check_password(password):
+            print("Passwords don't match")
+            raise PermissionDenied('Unauthorized')
+    
+        dt = datetime.now() + timedelta(days=7)
+
+
+        token = jwt.encode({ 'sub':  user_to_login.id, 'exp': int(dt.strftime('%s')) }, settings.SECRET_KEY, algorithm='HS256')
+        print('TOKEN ->', token)
+        
+        return Response({ 'message': f"Welcome back, {user_to_login.username}", 'token': token })
+    
