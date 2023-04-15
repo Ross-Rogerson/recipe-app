bb6f3acd138e6e490e3b10080d869db949796bf5
Ross Rogerson <ross.rogerson@hotmail.com>
2023-04-15 23:13:31 +0100

    unused code removed

--- ingredients/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import Ing
-
-# Any request hitting this urlpatterns list is: /api/sightings/
-# urlpatterns = [
-#     path('', SightingListView.as_view()), # /api/sightings/
-#     path('<int:pk>/', .as_view()) # /api/sightings/:pk
-# ]
